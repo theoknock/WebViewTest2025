6d28ffb8ab3d6b61e0bc8218b7d5382db35963b3
James Bush <jamesalanbush1972@gmail.com>
2025-11-14 16:36:21 -0500

    Initial Commit

---
+++ WebViewTest2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewTest2025
+//
+//  Created by Xcode Developer on 11/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewTest2025/WebViewTest2025App.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTest2025App.swift
+//  WebViewTest2025
+//
+//  Created by Xcode Developer on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTest2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
